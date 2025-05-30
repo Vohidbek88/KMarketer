6f317172dc342abc7158f0dbaaec98ef533d9256
Vohidbek88 <vohidabdunazarov88@gmail.com>
2025-05-30 15:06:51 +0500

    Kmarketer responsive landing page

---
+++ script.js
@@ -0,0 +1,25 @@
+const items = document.getElementById('items')
+const allLi = document.querySelectorAll('#items li')
+const see = document.querySelector('.overlay')
+const icons = document.querySelector('.icons')
+const nav = document.querySelector('nav')
+
+function toggleMenu() {
+       items.classList.toggle('show')
+       see.classList.toggle('see')
+       icons.classList.toggle('add-item')
+}
+
+allLi.forEach(item => {
+       item.addEventListener('click', () => {
+              toggleMenu()
+       })
+})
+
+window.addEventListener('scroll', () => {
+       if (window.scrollY > 40) {
+              nav.classList.add('scrolled');
+       } else {
+              nav.classList.remove('scrolled');
+       }
+})
