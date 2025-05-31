2cef4c6d4d05cd6289a81ba5241af8e5e8418143
Vohidbek88 <vohidabdunazarov88@gmail.com>
2025-05-31 14:41:46 +0500

    perf up

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const items = document.getElementById('items')
-const allLi = document.querySelectorAll('#items li')
-const see = document.querySelector('.overlay')
-const icons = document.querySelector('.icons')
-const nav = document.querySelector('nav')
+window.addEventListener('DOMContentLoaded', () => {
+       const items = document.getElementById('items')
+       const allLi = document.querySelectorAll('#items li')
+       const see = document.querySelector('.overlay')
+       const icons = document.querySelector('.icons')
+       const nav = document.querySelector('nav')
 
-function toggleMenu() {
-       items.classList.toggle('show')
-       see.classList.toggle('see')
-       icons.classList.toggle('add-item')
-}
-
-allLi.forEach(item => {
-       item.addEventListener('click', () => {
-              toggleMenu()
-       })
-})
-
-window.addEventListener('scroll', () => {
-       if (window.scrollY > 40) {
-              nav.classList.add('scrolled');
-       } else {
-              nav.classList.remove('scrolled');
+       function toggleMenu() {
+              items.classList.toggle('show')
+              see.classList.toggle('see')
+              icons.classList.toggle('add-item')
        }
+
+       allLi.forEach(item => {
+              item.addEventListener('click', () => {
+                     toggleMenu()
+              })
+       })
+
+       window.addEventListener('scroll', () => {
+              if (window.scrollY > 40) {
+                     nav.classList.add('scrolled');
+              } else {
+                     nav.classList.remove('scrolled');
+              }
+       })
 })
