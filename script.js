c0f7d042232e89f52f2d0979dcb5a0a9c7367701
Vohidbek88 <vohidabdunazarov88@gmail.com>
2025-05-31 16:58:57 +0500

    perf app

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
-       const items = document.getElementById('items')
-       const allLi = document.querySelectorAll('#items li')
-       const see = document.querySelector('.overlay')
-       const icons = document.querySelector('.icons')
-       const nav = document.querySelector('nav')
+const items = document.getElementById('items')
+const allLi = document.querySelectorAll('#items li')
+const see = document.querySelector('.overlay')
+const icons = document.querySelector('.icons')
+const nav = document.querySelector('nav')
 
-       function toggleMenu() {
-              items.classList.toggle('show')
-              see.classList.toggle('see')
-              icons.classList.toggle('add-item')
-       }
+function toggleMenu() {
+       items.classList.toggle('show')
+       see.classList.toggle('see')
+       icons.classList.toggle('add-item')
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+  
        allLi.forEach(item => {
               item.addEventListener('click', () => {
+                     nav.classList.add('scroll');
                      toggleMenu()
               })
        })
@@ -20,8 +22,64 @@ window.addEventListener('DOMContentLoaded', () => {
        window.addEventListener('scroll', () => {
               if (window.scrollY > 40) {
                      nav.classList.add('scrolled');
+                     nav.classList.add('scroll');
               } else {
                      nav.classList.remove('scrolled');
+                     nav.classList.remove('scroll');
               }
        })
+
+       gsap.registerPlugin(ScrollTrigger);
+
+       gsap.from("#clients", {
+         scrollTrigger: {
+           trigger: "#clients",
+           start: "top 80%", // qachon boshlansin
+           toggleActions: "play none none none"
+         },
+         y: 200,
+         opacity: 0,
+         duration: 1
+       });
+
+            gsap.from("#choose-us", {
+              scrollTrigger: {
+                trigger: "#choose-us",
+                start: "top 80%", // qachon boshlansin
+                toggleActions: "play none none none"
+              },
+              y: 200,
+              opacity: 0,
+              duration: 1
+            });
+            gsap.from("#service", {
+              scrollTrigger: {
+                trigger: "#service",
+                start: "top 80%", // qachon boshlansin
+                toggleActions: "play none none none"
+              },
+              y: 200,
+              opacity: 0,
+              duration: 1
+            });
+            gsap.from("#team", {
+              scrollTrigger: {
+                trigger: "#team",
+                start: "top 80%", // qachon boshlansin
+                toggleActions: "play none none none"
+              },
+              y: 200,
+              opacity: 0,
+              duration: 1
+            });
+            gsap.from("#contact", {
+              scrollTrigger: {
+                trigger: "#contact",
+                start: "top 80%", // qachon boshlansin
+                toggleActions: "play none none none"
+              },
+              y: 200,
+              opacity: 0,
+              duration: 1
+            });
 })
